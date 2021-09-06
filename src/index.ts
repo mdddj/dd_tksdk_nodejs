80a4fc0d841c82919edce8b99c1d65c019e4c0b1
mddd <413153189@qq.com>
2021-09-06 17:32:45 +0800

    新增接口类对象

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log("hello 梁典典")
+import ServerUtil from "./utils/ServerUtil";
+
+/**
+ * 接口访问类
+ */
+class DdTaokeSdk {
+
+    private util : ServerUtil = ServerUtil.getInstance()
+
+    /**
+     * 私有化类构造
+     * @constructor
+     * @private
+     */
+    private DdTaokeSdk() {
+    }
+
+    public static _instance: DdTaokeSdk
+
+    /**
+     * 接口实例
+     */
+    public static getInstance(): DdTaokeSdk {
+        return this._instance ?? new DdTaokeSdk()
+    }
+
+
+
+
+
+}
